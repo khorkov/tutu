7e703a9e4ea0ecb0203c8cc9488372f7f65adc4a
playrix <web.playrix@yandex.ru>
2017-07-28 12:31:59 +0300

    User has many tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,6 @@
 class Ticket < ApplicationRecord
+  belongs_to :start_station_id, class_name: 'RailwayStation', foreign_key: :start_station_id
+  belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id
+  belongs_to :train
+  belongs_to :user
 end
