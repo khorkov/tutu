c45c071051c965553aac60479f68c9725d60e677
playrix <web.playrix@yandex.ru>
2017-08-03 13:11:36 +0300

    Fix errors

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,5 +1,4 @@
-class RailwayStation < ApplicationRecor
-
+class RailwayStation < ApplicationRecord
   has_many :trains, foreign_key: :current_station_id
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,4 @@
 class Route < ApplicationRecord
-
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
   has_many :trains
