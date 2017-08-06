f7d9ea49be23b736e2d9bff4d955fe1b8b0a6353
playrix <web.playrix@yandex.ru>
2017-08-06 22:24:50 +0300

    Create migration for wagons

--- db/migrate/20170806184916_create_wagons.rb
+++ db/migrate/20170806184916_create_wagons.rb
@@ -1,6 +1,10 @@
 class CreateWagons < ActiveRecord::Migration[5.1]
   def change
     create_table :wagons do |t|
+      t.integer :number
+      t.string :wagon_type
+      t.integer :bottom_seats
+      t.integer :top_seats
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728112945) do
+ActiveRecord::Schema.define(version: 20170806184916) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 20170728112945) do
 
   create_table "users", force: :cascade do |t|
     t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wagons", force: :cascade do |t|
+    t.integer "number"
+    t.string "wagon_type"
+    t.integer "bottom_seats"
+    t.integer "top_seats"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
