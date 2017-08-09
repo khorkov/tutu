b1f7bcab5768a5b52a27bf70263a0e347961679a
playrix <web.playrix@yandex.ru>
2017-08-09 19:00:45 +0300

    Add to show extra field

--- app/controllers/wagons_controller.rb
+++ app/controllers/wagons_controller.rb
@@ -43,6 +43,6 @@ class WagonsController < ApplicationController
   end
 
   def wagon_params
-    params.require(:wagon).permit(:number, :wagon_type, :bottom_seats, :top_seats)
+    params.require(:wagon).permit(:number, :wagon_type, :bottom_seats, :top_seats, :train_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806184916) do
+ActiveRecord::Schema.define(version: 20170809155200) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20170806184916) do
     t.integer "top_seats"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "train_id"
+    t.index ["train_id"], name: "index_wagons_on_train_id"
   end
 
 end
