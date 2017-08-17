f99f900b25b3bb22411146d39e324c1f0da7086e
playrix <web.playrix@yandex.ru>
2017-08-18 00:59:59 +0300

    Fix: The number is automatically assigned to the wagon

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -5,4 +5,12 @@ class Wagon < ApplicationRecord
   belongs_to :train, optional: true
 
   validates :number, :wagon_type, :bottom_seats, :top_seats, presence: true
+
+  before_validation :set_number
+
+  private
+
+  def set_number
+    self.number ||= train.wagons.maximum('number').to_i + 1
+  end
 end
