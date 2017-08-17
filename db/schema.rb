26e85689e75cf43780388ba7703c67a88f154494
playrix <web.playrix@yandex.ru>
2017-08-17 17:22:43 +0300

    Fix: RailwayStation station position in the route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809155200) do
+ActiveRecord::Schema.define(version: 20170812175332) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170809155200) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+    t.integer "station_position", default: 0
   end
 
   create_table "routes", force: :cascade do |t|
